d6d6feb21d8418f70e00fe5ac94e173b4856ef87
Coen Jacobs <coenjacobs@gmail.com>
2016-06-15 20:49:18 +0200

    Fixed page ids for stylesheet loading

--- src/Admin/Admin.php
+++ src/Admin/Admin.php
@@ -44,7 +44,7 @@ class Admin {
 
 		$screen = get_current_screen();
 		
-		if ( in_array( $screen->id, apply_filters( 'job_manager_admin_screen_ids', array( 'edit-job_listing', 'job_listing', 'job_listing_page_job-manager-settings', 'job_listing_page_job-manager-addons' ) ) ) ) {
+		if ( in_array( $screen->id, apply_filters( 'job_manager_admin_screen_ids', array( 'edit-job_listing', 'job_listing', 'listings_page_listings-settings', 'listings_page_listings-addons' ) ) ) ) {
 			$jquery_version = isset( $wp_scripts->registered['jquery-ui-core']->ver ) ? $wp_scripts->registered['jquery-ui-core']->ver : '1.9.2';
 
 			wp_enqueue_style( 'jquery-ui-style', '//code.jquery.com/ui/' . $jquery_version . '/themes/smoothness/jquery-ui.css', array(), $jquery_version );
