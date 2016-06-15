df0a24a17ccd0112ab5d3f0a1279a1e5829a348b
Coen Jacobs <coenjacobs@gmail.com>
2016-06-15 15:02:46 +0200

    Final bits changed in main plugin file

--- listings.php
+++ listings.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * Plugin Name: Listings
- * Description: Manage job listings from the WordPress admin panel, and allow users to post jobs directly to your site.
+ * Description: Manage listings from the WordPress admin panel, and allow users to listed items directly to your site.
  * Version: 1.0.0
  * Author: The Look and Feel
  * Text Domain: listings
  */
 
 // Define constants
-define( 'LISTINGS_VERSION', '1.25.0' );
+define( 'LISTINGS_VERSION', '1.0.0' );
 define( 'LISTINGS_PLUGIN_DIR', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 define( 'LISTINGS_PLUGIN_URL', untrailingslashit( plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) ) );
 
