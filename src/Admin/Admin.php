ec6a25d80912afdd209c019cf40f5e52310ae2bf
Coen Jacobs <coenjacobs@gmail.com>
2016-06-17 18:07:40 +0200

    Always load the admin css

--- src/Admin/Admin.php
+++ src/Admin/Admin.php
@@ -43,12 +43,13 @@ class Admin {
 		global $wp_scripts;
 
 		$screen = get_current_screen();
+
+		wp_enqueue_style( 'job_manager_admin_css', LISTINGS_PLUGIN_URL . '/assets/css/admin.css' );
 		
 		if ( in_array( $screen->id, apply_filters( 'job_manager_admin_screen_ids', array( 'edit-job_listing', 'job_listing', 'listings_page_listings-settings', 'listings_page_listings-addons' ) ) ) ) {
 			$jquery_version = isset( $wp_scripts->registered['jquery-ui-core']->ver ) ? $wp_scripts->registered['jquery-ui-core']->ver : '1.9.2';
 
 			wp_enqueue_style( 'jquery-ui-style', '//code.jquery.com/ui/' . $jquery_version . '/themes/smoothness/jquery-ui.css', array(), $jquery_version );
-			wp_enqueue_style( 'job_manager_admin_css', LISTINGS_PLUGIN_URL . '/assets/css/admin.css' );
 			wp_register_script( 'jquery-tiptip', LISTINGS_PLUGIN_URL. '/assets/js/jquery-tiptip/jquery.tipTip.min.js', array( 'jquery' ), LISTINGS_VERSION, true );
 			wp_enqueue_script( 'job_manager_admin_js', LISTINGS_PLUGIN_URL. '/assets/js/admin.min.js', array( 'jquery', 'jquery-tiptip', 'jquery-ui-datepicker' ), LISTINGS_VERSION, true );
 
