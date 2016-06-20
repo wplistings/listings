f71e938bdef33e29b67f15d95120325eb4f17575
Coen Jacobs <coenjacobs@gmail.com>
2016-06-20 21:41:05 +0200

    Moved jobs specific functions to jobs plugin

--- helpers/functions.php
+++ helpers/functions.php
@@ -213,23 +213,6 @@ function get_featured_job_ids() {
 }
 endif;
 
-if ( ! function_exists( 'get_job_listing_types' ) ) :
-/**
- * Get job listing types
- *
- * @access public
- * @return array
- */
-function get_job_listing_types( $fields = 'all' ) {
-	return get_terms( "job_listing_type", array(
-		'orderby'    => 'name',
-		'order'      => 'ASC',
-		'hide_empty' => false,
-		'fields'     => $fields
-	) );
-}
-endif;
-
 if ( ! function_exists( 'get_job_listing_categories' ) ) :
 /**
  * Get job categories
@@ -247,70 +230,6 @@ function get_job_listing_categories() {
 	    'order'         => 'ASC',
 	    'hide_empty'    => false,
 	) );
-}
-endif;
-
-if ( ! function_exists( 'job_manager_get_filtered_links' ) ) :
-/**
- * Shows links after filtering jobs
- */
-function job_manager_get_filtered_links( $args = array() ) {
-	$job_categories = array();
-	$types          = get_job_listing_types();
-
-	// Convert to slugs
-	if ( $args['search_categories'] ) {
-		foreach ( $args['search_categories'] as $category ) {
-			if ( is_numeric( $category ) ) {
-				$category_object = get_term_by( 'id', $category, 'job_listing_category' );
-				if ( ! is_wp_error( $category_object ) ) {
-					$job_categories[] = $category_object->slug;
-				}
-			} else {
-				$job_categories[] = $category;
-			}
-		}
-	}
-
-	$links = apply_filters( 'job_manager_job_filters_showing_jobs_links', array(
-		'reset' => array(
-			'name' => __( 'Reset', 'wp-job-manager' ),
-			'url'  => '#'
-		),
-		'rss_link' => array(
-			'name' => __( 'RSS', 'wp-job-manager' ),
-			'url'  => get_job_listing_rss_link( apply_filters( 'job_manager_get_listings_custom_filter_rss_args', array(
-				'job_types'       => isset( $args['filter_job_types'] ) ? implode( ',', $args['filter_job_types'] ) : '',
-				'search_location' => $args['search_location'],
-				'job_categories'  => implode( ',', $job_categories ),
-				'search_keywords' => $args['search_keywords'],
-			) ) )
-		)
-	), $args );
-
-	if ( sizeof( $args['filter_job_types'] ) === sizeof( $types ) && ! $args['search_keywords'] && ! $args['search_location'] && ! $args['search_categories'] && ! apply_filters( 'job_manager_get_listings_custom_filter', false ) ) {
-		unset( $links['reset'] );
-	}
-
-	$return = '';
-
-	foreach ( $links as $key => $link ) {
-		$return .= '<a href="' . esc_url( $link['url'] ) . '" class="' . esc_attr( $key ) . '">' . $link['name'] . '</a>';
-	}
-
-	return $return;
-}
-endif;
-
-if ( ! function_exists( 'get_job_listing_rss_link' ) ) :
-/**
- * Get the Job Listing RSS link
- *
- * @return string
- */
-function get_job_listing_rss_link( $args = array() ) {
-	$rss_link = add_query_arg( urlencode_deep( array_merge( array( 'feed' => 'job_feed' ), $args ) ), home_url() );
-	return $rss_link;
 }
 endif;
 
