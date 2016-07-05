3e96428547044092bbf1bcc590753e70b74143cf
Coen Jacobs <coenjacobs@gmail.com>
2016-07-05 11:28:23 +0200

    Generic template logic for core post type

--- src/PostTypes.php
+++ src/PostTypes.php
@@ -10,20 +10,20 @@ class PostTypes {
     public function __construct() {
         add_action( 'init', array( $this, 'register_post_types' ), 0 );
 
-        add_filter( 'the_job_description', 'wptexturize'        );
-        add_filter( 'the_job_description', 'convert_smilies'    );
-        add_filter( 'the_job_description', 'convert_chars'      );
-        add_filter( 'the_job_description', 'wpautop'            );
-        add_filter( 'the_job_description', 'shortcode_unautop'  );
-        add_filter( 'the_job_description', 'prepend_attachment' );
+        add_filter( 'listings_single_description', 'wptexturize'        );
+        add_filter( 'listings_single_description', 'convert_smilies'    );
+        add_filter( 'listings_single_description', 'convert_chars'      );
+        add_filter( 'listings_single_description', 'wpautop'            );
+        add_filter( 'listings_single_description', 'shortcode_unautop'  );
+        add_filter( 'listings_single_description', 'prepend_attachment' );
 
         if ( ! empty( $GLOBALS['wp_embed'] ) ) {
-            add_filter( 'the_job_description', array( $GLOBALS['wp_embed'], 'run_shortcode' ), 8 );
-            add_filter( 'the_job_description', array( $GLOBALS['wp_embed'], 'autoembed' ), 8 );
+            add_filter( 'listings_single_description', array( $GLOBALS['wp_embed'], 'run_shortcode' ), 8 );
+            add_filter( 'listings_single_description', array( $GLOBALS['wp_embed'], 'autoembed' ), 8 );
         }
 
         // Single job content
-        $this->job_content_filter( true );
+        $this->listing_content_filter( true );
     }
 
     /**
@@ -42,14 +42,8 @@ class PostTypes {
         $singular  = __( 'Listing', 'listings' );
         $plural    = __( 'Listings', 'listings' );
 
-        if ( current_theme_supports( 'job-manager-templates' ) ) {
-            $has_archive = _x( 'jobs', 'Post type archive slug - resave permalinks after changing this', 'listings' );
-        } else {
-            $has_archive = false;
-        }
-
         $rewrite     = array(
-            'slug'       => _x( 'listings', 'Job permalink - resave permalinks after changing this', 'listings' ),
+            'slug'       => _x( 'listings', 'Listing permalink - resave permalinks after changing this', 'listings' ),
             'with_front' => false,
             'feeds'      => true,
             'pages'      => false
@@ -89,32 +83,27 @@ class PostTypes {
                 'rewrite' 				=> $rewrite,
                 'query_var' 			=> true,
                 'supports' 				=> array( 'title', 'editor', 'custom-fields', 'publicize', 'thumbnail' ),
-                'has_archive' 			=> $has_archive,
+                'has_archive' 			=> _x( 'listings', 'Post type archive slug - resave permalinks after changing this', 'listings' ),
                 'show_in_nav_menus' 	=> true
             ) )
         );
-
-        /**
-         * Feeds
-         */
-        add_feed( 'listing_feed', array( $this, 'listing_feed' ) );
     }
 
     /**
      * Toggle filter on and off
      */
-    private function job_content_filter( $enable ) {
+    private function listing_content_filter( $enable ) {
         if ( ! $enable ) {
-            remove_filter( 'the_content', array( $this, 'job_content' ) );
+            remove_filter( 'the_content', array( $this, 'listing_content' ) );
         } else {
-            add_filter( 'the_content', array( $this, 'job_content' ) );
+            add_filter( 'the_content', array( $this, 'listing_content' ) );
         }
     }
 
     /**
      * Add extra content before/after the post for single job listings.
      */
-    public function job_content( $content ) {
+    public function listing_content( $content ) {
         global $post;
 
         if ( ! is_singular( 'listing' ) || ! in_the_loop() || 'listing' !== $post->post_type ) {
@@ -123,28 +112,16 @@ class PostTypes {
 
         ob_start();
 
-        $this->job_content_filter( false );
+        $this->listing_content_filter( false );
 
         do_action( 'listing_content_start' );
 
-        listings_get_template_part( 'content-single', 'job_listing' );
+        listings_get_template_part( 'content-single', 'listing' );
 
         do_action( 'listing_content_end' );
 
-        $this->job_content_filter( true );
+        $this->listing_content_filter( true );
 
-        return apply_filters( 'listings_single_listing_content', ob_get_clean(), $post );
-    }
-
-    /**
-     * Fix post name when wp_update_post changes it
-     * @param  array $data
-     * @return array
-     */
-    public function fix_post_name( $data, $postarr ) {
-        if ( 'listing' === $data['post_type'] && 'pending' === $data['post_status'] && ! current_user_can( 'publish_posts' ) ) {
-            $data['post_name'] = $postarr['post_name'];
-        }
-        return $data;
+        return apply_filters( 'listings_single_content', ob_get_clean(), $post );
     }
 }
