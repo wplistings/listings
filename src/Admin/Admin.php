7eebede361ab272af9a6d4b1c899117ca7d91e8b
Coen Jacobs <coenjacobs@gmail.com>
2016-07-08 11:09:09 +0200

    Use core plugin screen ids for enqueuing global scripts

--- src/Admin/Admin.php
+++ src/Admin/Admin.php
@@ -40,8 +40,8 @@ class Admin {
 		$screen = get_current_screen();
 
 		wp_enqueue_style( 'listings_admin_css', LISTINGS_PLUGIN_URL . '/assets/css/admin.css' );
-		
-		if ( in_array( $screen->id, apply_filters( 'listings_admin_screen_ids', array( 'edit-job_listing', 'job_listing', 'listings_page_listings-settings', 'listings_page_listings-addons' ) ) ) ) {
+
+		if ( in_array( $screen->id, apply_filters( 'listings_admin_screen_ids', array( 'edit-listing', 'listing', 'listings_page_listings-settings' ) ) ) ) {
 			$jquery_version = isset( $wp_scripts->registered['jquery-ui-core']->ver ) ? $wp_scripts->registered['jquery-ui-core']->ver : '1.9.2';
 
 			wp_enqueue_style( 'jquery-ui-style', '//code.jquery.com/ui/' . $jquery_version . '/themes/smoothness/jquery-ui.css', array(), $jquery_version );
