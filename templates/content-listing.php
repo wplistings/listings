3b25841d6e17807020bcbe81f9a6995c0eb81bf3
Coen Jacobs <coenjacobs@gmail.com>
2016-07-12 20:30:39 +0200

    Improved markup for single listing in list

--- templates/content-listing.php
+++ templates/content-listing.php
@@ -1,15 +1,32 @@
 <?php global $post; ?>
 <li>
-    <a href="<?php echo get_permalink($post->ID); ?>">
-        <div class="title">
-            <h3><?php the_title(); ?></h3>
-        </div>
+   <?php $permalink = get_permalink($post->ID); ?>
+    <div class="thumbnail">
+        <a href="<?php echo $permalink; ?>">
+            <?php
+            echo get_the_post_thumbnail($post->ID, 'thumbnail');
+            ?>
+        </a>
+    </div>
+    <div class="body">
+        <h3><a href="<?php echo $permalink; ?>">
+            <?php the_title(); ?>
+        </a></h3>
         <div class="meta">
             <?php do_action( 'listings_list_meta_start' ); ?>
 
-            <span class="date"><date><?php printf( __( '%s ago', 'listings' ), human_time_diff( get_post_time( 'U' ), current_time( 'timestamp' ) ) ); ?></date></span>
+            <?php
+            $categories = wp_get_post_terms($post->ID, 'listings_category');
+            $categories_array = array_map(function($item) {
+                $permalink = get_term_link($item);
+                return '<a href="'.$permalink.'">'.$item->name.'</a>';
+            }, $categories);
+            $categories_string = implode(', ', $categories_array); ?>
+
+            <span class="date"><date><?php printf( __( 'Posted %s ago', 'listings' ), human_time_diff( get_post_time( 'U' ), current_time( 'timestamp' ) ) ); ?></date></span>
+            <span class="categories">in <?php echo $categories_string ?></span>
 
             <?php do_action( 'listings_list_meta_end' ); ?>
         </div>
-    </a>
+    </div>
 </li>
