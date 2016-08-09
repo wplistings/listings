ca52075552810498216f66fa6bf2e640a7c8bf3f
Subharanjan Mantri <subharanjanmantri@gmail.com>
2016-08-09 13:45:21 +0530

    Fixes the issue widget cache not getting cleared when deleting post

--- src/Widgets/Widget.php
+++ src/Widgets/Widget.php
@@ -31,7 +31,7 @@ abstract class Widget extends WP_Widget {
         parent::__construct( $this->widget_id, $this->widget_name, $widget_ops );
 
         add_action( 'save_post', array( $this, 'flush_widget_cache' ) );
-        add_action( 'deleted_post', array( $this, 'flush_widget_cache' ) );
+        add_action( 'delete_post', array( $this, 'flush_widget_cache' ) );
         add_action( 'switch_theme', array( $this, 'flush_widget_cache' ) );
     }
 
